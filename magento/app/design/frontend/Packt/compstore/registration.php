3209878ea256b9cff7f8c71ca528f5d0b4705fda
Manhtq <trinhquocmanh1990@gmail.com>
2022-02-16 14:42:08 +0700

    add new compstore theme

--- magento/app/design/frontend/Packt/compstore/registration.php
+++ magento/app/design/frontend/Packt/compstore/registration.php
@@ -1,10 +1,6 @@
 <?php
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Packt/basic',
+    'frontend/Packt/compstore',
     __DIR__
 );
